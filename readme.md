c37f9928c2f42fbf079b265d879c0358bb1c3753
odus-ex <at.infosec@protonmail.com>
2022-06-17 08:21:29 +0530

    Example and readme changes

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 - Snack order is preserved
 - One place to add all your snackbars
 
-- minimum time for autohide
+- minimum time for autohide is 1sec
---
+++ src/config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+  styleTobgColorClassMap: {
+    success: "bg_success",
+    error: "bg_error",
+    info: "bg_info",
+    warning: "bg_warning",
+  },
+  defaultAutoTimeout: 3000,
+  minimumAutoTimeout: 1000,
+};
+
+export default CONFIG;
